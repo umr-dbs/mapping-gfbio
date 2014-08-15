2f8733c149d006641c47604bd5235d70fd8f9cec
beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2014-08-15 14:35:42 +0200

    added range metadata to XYGraph

--- mapping/raster/xygraph.h
+++ mapping/raster/xygraph.h
@@ -13,9 +13,22 @@
 template<std::size_t dimensions>
 class XYGraph : public DataVector {
 	public:
-		XYGraph() {};
+		XYGraph() {
+			rangeMin.fill(std::numeric_limits<double>::max());
+			rangeMax.fill(std::numeric_limits<double>::min());
+		};
 
-		auto addPoint(std::array<double, dimensions> point) -> void { points.push_back(point); sorted = false; }
+		auto addPoint(std::array<double, dimensions> point) -> void {
+			points.push_back(point);
+			sorted = false;
+
+			for (std::size_t index = 0; index < dimensions; ++index) {
+				if(point[index] < rangeMin[index])
+					rangeMin[index] = point[index];
+				if(point[index] > rangeMax[index])
+					rangeMax[index] = point[index];
+			}
+		}
 		auto incNoData() -> void { nodata_count++; }
 
 		auto sort() -> void { std::sort(points.begin(), points.end(), sortFunction); sorted = true;	}
@@ -26,8 +39,12 @@ class XYGraph : public DataVector {
 
 			std::stringstream buffer;
 			buffer << "{\"type\": \"xygraph\", ";
-			buffer << "\"metadata\": {\"dimensions\": " << dimensions << ", \"nodata\": " << nodata_count << ", \"numberOfPoints\": " << points.size() << "}, ";
-			buffer << "\"data\": [";
+			buffer << "\"metadata\": {\"dimensions\": " << dimensions << ", \"nodata\": " << nodata_count << ", \"numberOfPoints\": " << points.size() << ", \"range\": [";
+			for (std::size_t index = 0; index < dimensions; ++index) {
+				buffer << "[" << rangeMin[index] << "," << rangeMax[index] << "],";
+			}
+			buffer.seekp(((long) buffer.tellp()) - 1);
+			buffer << "]}, " << "\"data\": [";
 			for(std::array<double, dimensions>& point : points) {
 				buffer << "[";
 				for(double& element : point) {
@@ -45,6 +62,9 @@ class XYGraph : public DataVector {
 	private:
 		std::vector<std::array<double, dimensions>> points;
 		std::size_t nodata_count{0};
+
+		std::array<double, dimensions> rangeMin;
+		std::array<double, dimensions> rangeMax;
 
 		bool sorted{false};
 		static auto sortFunction(const std::array<double, dimensions>& e1,	const std::array<double, dimensions>& e2) -> bool {

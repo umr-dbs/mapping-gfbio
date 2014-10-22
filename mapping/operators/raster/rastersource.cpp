17185465588ef8c7fb210563523c16d789c95bb9
Christian Authmann <authmanc@mathematik.uni-marburg.de>
2014-10-22 09:02:43 +0200

    re-introduce specifying raster sources by full path; for testcases

--- mapping/operators/raster/rastersource.cpp
+++ mapping/operators/raster/rastersource.cpp
@@ -28,7 +28,18 @@ class SourceOperator : public GenericOperator {
 // RasterSource Operator
 SourceOperator::SourceOperator(int sourcecounts[], GenericOperator *sources[], Json::Value &params) : GenericOperator(sourcecounts, sources), rastersource(nullptr) {
 	assumeSources(0);
-	std::string filename = std::string("datasources/") + params.get("sourcename", "").asString() + std::string(".json");
+	std::string fullpath = params.get("sourcepath", "").asString();
+	std::string sourcename = params.get("sourcename", "").asString();
+	if (fullpath.length() > 0 && sourcename.length() > 0)
+		throw OperatorException("SourceOperator: specify sourcepath or sourcename, not both");
+	if (fullpath.length() == 0 && sourcename.length() == 0)
+		throw OperatorException("SourceOperator: missing sourcepath or sourcename");
+	std::string filename;
+	if (fullpath.length() > 0)
+		filename = fullpath;
+	else
+		filename = std::string("datasources/") + sourcename + std::string(".json");
+
 	rastersource = RasterSourceManager::open(filename.c_str());
 	channel = params.get("channel", 0).asInt();
 	transform = params.get("transform", true).asBool();

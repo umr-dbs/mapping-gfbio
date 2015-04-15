b54adedc7c89db46841c5c113e137c7ca3381f08
droenner <droenner@Mathematik.Uni-Marburg.DE>
2015-04-15 10:30:31 +0200

    enabled more OpenCL more comments some TODOs more default values

--- mapping/operators/msat/gccthermthresholddetection.cpp
+++ mapping/operators/msat/gccthermthresholddetection.cpp
@@ -23,18 +23,18 @@ class MSATGccThermThresholdDetectionOperator : public GenericOperator {
 
 	private:
 		double findGccThermThreshold(Histogram *histogram);
-		const double bucket_size{1.0/2.0}; //this is the bucket size used for the histogram(s)
-		const int minimum_increasing_buckets_for_rising_trend{3}; //threshold detection phase 1 uses this to determine the minimum between cloud and land peak
-		const int minimum_soft_falling_buckets{2}; //threshold detection phase 2 uses this to detect a cloud peak merged into the land peak
-		const double minimum_land_peak_temperature{-1}; //this is the minimum land peak temperature
-		const double minimum_cloud_threshold_temperature{-12}; //this is the minimum valid cloud threshold temperature
+		const double bucket_size{1.0/2.0}; //this is the bucket size used for the histogram(s) //default: 1/3 -> this is problematic as the BBT is now calculated using the Eumetsat LUT with steps like .0 .5 .0 .5 .0 .5 ...
+		const int minimum_increasing_buckets_for_rising_trend{3}; //threshold detection phase 1 uses this to determine the minimum between cloud and land peak //default:3
+		const int minimum_soft_falling_buckets{3}; //threshold detection phase 2 uses this to detect a cloud peak merged into the land peak //default:2
+		const double minimum_land_peak_temperature{-5}; //this is the minimum land peak temperature. //default:-1
+		const double minimum_cloud_threshold_temperature{-12}; //this is the minimum valid cloud threshold temperature //default:-12
 		const double cloud_minimum_and_peak_ratio{0.85}; //ratio to determine if the minimum between land and cloud peak is ?distinct?
-		const double merged_peaks_bucket_ratio_bound_lower{0.95}; //default is 0.8
-		const double merged_peaks_bucket_ratio_bound_higher{1.05}; //default is 1
+		const double merged_peaks_bucket_ratio_bound_lower{0.8}; //default is 0.8
+		const double merged_peaks_bucket_ratio_bound_higher{1.00}; //default is 1
 };
 
 
-#include "operators/msat/classification_kernels.cl.h"
+#include "operators/raster/classification_kernels.cl.h"
 
 MSATGccThermThresholdDetectionOperator::MSATGccThermThresholdDetectionOperator(int sourcecounts[], GenericOperator *sources[], Json::Value &params) : GenericOperator(sourcecounts, sources) {
 	assumeSources(1);
@@ -137,16 +137,16 @@ std::pair<int,int> phase1FindMinimumWithRisingTrendBetweenLandAndCloudPeak(Histo
 	 **/
 	for(int i = land_peak_bucket-1; i >= 0; i--){
 		int bucket_count = histogram->getCountForBucket(i);
-		   if(bucket_count > histogram->getCountForBucket(i+1)){
-		   increasing_buckets += 1;
-		  if(increasing_buckets >= minimum_increasing_buckets_for_rising_trend)
-			   break;
-	   }
-	   else{
-		   increasing_buckets = std::max(increasing_buckets-1, 0);
-		   if(bucket_count < histogram->getCountForBucket(minimum_between_land_and_cloud_peak_bucket))
-			   minimum_between_land_and_cloud_peak_bucket = i;
-	   }
+		if(bucket_count > histogram->getCountForBucket(i+1)){
+			increasing_buckets += 1;
+			if(increasing_buckets >= minimum_increasing_buckets_for_rising_trend)
+				break;
+		}
+		else{
+			increasing_buckets = std::max(increasing_buckets-1, 0);
+			if(bucket_count < histogram->getCountForBucket(minimum_between_land_and_cloud_peak_bucket))
+				 minimum_between_land_and_cloud_peak_bucket = i;
+		}
 	}
 	return (std::pair<int,int>{minimum_between_land_and_cloud_peak_bucket, increasing_buckets});
 }
@@ -224,18 +224,18 @@ double MSATGccThermThresholdDetectionOperator::findGccThermThreshold(Histogram *
 	double cloud_peak_value = histogram->calculateBucketLowerBorder(cloud_peak_bucket);
 	double merged_peeks_soft_falling_value = histogram->calculateBucketLowerBorder(merged_peeks_soft_falling_bucket);
 
-	/** check if the minimum is a valid cloud threshold **/
+	/** check if the minimum is a valid cloud threshold **/ //TODO: refactor as function
 	bool cant_use_minimum_as_cloud_threshold =	(increasing_buckets < minimum_increasing_buckets_for_rising_trend) | // rising trends was not met TODO: this is always covered by the next check and could be removed (if the histogram is not to small)
 											(minimum_between_land_and_cloud_peak_value < minimum_cloud_threshold_temperature) | // cloud_threshold is to cold
 											((cloud_peak_bucket != minimum_between_land_and_cloud_peak_bucket) & ((minimum_between_land_and_cloud_peak_value/cloud_peak_value)>cloud_minimum_and_peak_ratio)); //exp: cloud area is to flat
 
-	/**add annotations for peaks and minimum **/
+	/**add annotations for peaks and minimum to the histogram**/
 	histogram->addMarker(land_peak_value, "landpeak: "+std::to_string(land_peak_value)+" bucket: "+std::to_string(land_peak_bucket));
 	histogram->addMarker(minimum_between_land_and_cloud_peak_value, "minimum: "+std::to_string(minimum_between_land_and_cloud_peak_value)+" bucket: "+std::to_string(minimum_between_land_and_cloud_peak_bucket)+" cant use: "+std::to_string(cant_use_minimum_as_cloud_threshold));
 	histogram->addMarker(cloud_peak_value, "cloudpeak: "+std::to_string(cloud_peak_value)+" bucket: "+std::to_string(cloud_peak_bucket));
 	histogram->addMarker(merged_peeks_soft_falling_value, "mergedpeeks soft falling: "+std::to_string(merged_peeks_soft_falling_value)+" bucket: "+std::to_string(merged_peeks_soft_falling_bucket));
 
-	/** now decide if we shoud use the minimum between peeks or the merged peeks value**/
+	/** now decide if we should use the minimum between peeks or the merged peeks value**/
 	if(cant_use_minimum_as_cloud_threshold){
 		return (merged_peeks_soft_falling_value); //return merged peeks value
 	}else{
@@ -283,26 +283,27 @@ std::unique_ptr<GenericRaster> MSATGccThermThresholdDetectionOperator::getRaster
 	solar_zenith_angle_raster->setRepresentation(GenericRaster::Representation::CPU); //TODO: do GPU
 
 	//create the output raster
-	double min = -12;std::min(temperature_threshold_day, temperature_threshold_night);
-	double max = 12;std::max(temperature_threshold_day, temperature_threshold_night);
+	double min = std::min(temperature_threshold_day, temperature_threshold_night);
+	double max = std::max(temperature_threshold_day, temperature_threshold_night);
 	DataDescription out_dd(GDT_Float32, min, max); // no no_data //raster->dd.has_no_data, output_no_data);
 	out_dd.addNoData();
-	auto raster_out = GenericRaster::create(out_dd, *solar_zenith_angle_raster, GenericRaster::Representation::CPU);
+	auto raster_out = GenericRaster::create(out_dd, *solar_zenith_angle_raster, GenericRaster::Representation::OPENCL);
 
-	//TODO: figure out why opencl kernel is not working. NOTE: kernel code does no longer match to the used data structures!
-	//RasterOpenCL::CLProgram prog;
-	//	prog.setProfiler(profiler);
-	//	prog.addOutRaster(raster_out.get());
-	//	prog.addInRaster(solar_zenith_angle_raster.get());
-	//	prog.compile(operators_msat_classification_kernels, "valueClassificationKernel");
-	//	prog.addArg(classification_bounds_lower);
-	//	prog.addArg(classification_bounds_upper);
-	//	prog.addArg(classes);
-	//	prog.addArg(2);
-	//	prog.run();
+	//Use the OpenCL classification kernel to fill a raster with the values
+	RasterOpenCL::CLProgram prog;
+	prog.setProfiler(profiler);
+	prog.addOutRaster(raster_out.get());
+	prog.addInRaster(solar_zenith_angle_raster.get());
+	prog.compile(operators_raster_classification_kernels, "replacementByRangeKernel");
+	prog.addArg(classification_bounds_lower);
+	prog.addArg(classification_bounds_upper);
+	prog.addArg(classification_classes);
+	prog.addArg(2);
+	prog.addArg(static_cast<float>(out_dd.no_data)); //keep nodata as nodata
+	prog.run();
 
 
-	callBinaryOperatorFunc<RasterClassification>(solar_zenith_angle_raster.get(), raster_out.get(), classification_bounds_lower, classification_bounds_upper, classification_classes);
+	//callBinaryOperatorFunc<RasterClassification>(solar_zenith_angle_raster.get(), raster_out.get(), classification_bounds_lower, classification_bounds_upper, classification_classes);
 	return (raster_out);
 }
 

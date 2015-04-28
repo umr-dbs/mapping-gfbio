306572b884f000873eba90beb2e05c73976e5d24
Christian Authmann <authmanc@mathematik.uni-marburg.de>
2015-04-28 16:04:34 +0200

    attributes from raster: first prototype of temporal correlation, only correlating by starting timestamp of the points.

--- mapping/operators/combined/raster_metadata_to_points.cpp
+++ mapping/operators/combined/raster_metadata_to_points.cpp
@@ -1,6 +1,7 @@
 #include "datatypes/raster.h"
 #include "datatypes/raster/raster_priv.h"
 #include "datatypes/raster/typejuggling.h"
+#include "datatypes/pointcollection.h"
 #include "raster/profiler.h"
 #include "raster/opencl.h"
 #include "operators/operator.h"
@@ -10,7 +11,8 @@
 #include <json/json.h>
 #include <algorithm>
 #include <vector>
-#include "datatypes/pointcollection.h"
+#include <utility>
+
 
 class RasterMetaDataToPoints: public GenericOperator {
 	public:
@@ -107,15 +109,62 @@ std::unique_ptr<PointCollection> RasterMetaDataToPoints::getPointCollection(cons
 	auto points = getPointCollectionFromSource(0, rect, profiler, FeatureCollectionQM::SINGLE_ELEMENT_FEATURES);
 
 	if (points->hasTime()) {
-		// TODO: sort by time, iterate over all timestamps, fetch the correct raster, then add metadata
-		throw OperatorException("raster_metadata_to_points: Cannot yet handle PointCollections with timestamps");
-	}
+		// sort by time, iterate over all timestamps, fetch the correct raster, then add the attribute
+		// TODO: currently, missing rasters will just throw an exception. Maybe we should add NaN instead?
+		using p = std::pair<size_t, double>;
+		std::vector< p > temporal_index;
+		auto featurecount = points->getFeatureCount();
+		temporal_index.reserve(featurecount);
+		for (size_t i=0;i<featurecount;i++)
+			temporal_index.emplace_back(i, points->time_start[i]);
 
-	auto rasters = getRasterSourceCount();
-	for (int r=0;r<rasters;r++) {
-		auto raster = getRasterFromSource(r, rect, profiler);
-		Profiler::Profiler p("RASTER_METADATA_TO_POINTS_OPERATOR");
-		enhance(*points, *raster, names.at(r), profiler);
+		std::sort(temporal_index.begin(), temporal_index.end(), [=] (const p &a, const p&b) -> bool {
+			return a.second < b.second;
+		});
+
+		auto rasters = getRasterSourceCount();
+		TemporalReference tref = TemporalReference::unreferenced();
+		for (int r=0;r<rasters;r++) {
+			auto &attributevector = points->local_md_value.addVector(names.at(r), featurecount);
+			// iterate over time
+			size_t current_idx = 0;
+			while (current_idx < featurecount) {
+				QueryRectangle rect2 = rect;
+				rect2.timestamp = temporal_index[current_idx].second;
+				auto raster = getRasterFromSource(r, rect2, profiler);
+				while (current_idx < featurecount && temporal_index[current_idx].second < raster->stref.t2) {
+					// load point and add
+					auto featureidx = temporal_index[current_idx].first;
+					const auto &c = points->coordinates[featureidx];
+
+					auto rasterCoordinateX = raster->WorldToPixelX(c.x);
+					auto rasterCoordinateY = raster->WorldToPixelY(c.y);
+					if (rasterCoordinateX >= 0 && rasterCoordinateY >= 0 &&	rasterCoordinateX < raster->width && rasterCoordinateY < raster->height) {
+						double value = raster->getAsDouble(rasterCoordinateX, rasterCoordinateY);
+						if (!raster->dd.is_no_data(value)) {
+							printf("Setting %lu to %f from raster %f -> %f\n", featureidx, value, raster->stref.t1, raster->stref.t2);
+							attributevector[featureidx] = value;
+						}
+					}
+
+					current_idx++;
+				}
+			}
+		}
+	}
+	else {
+		auto rasters = getRasterSourceCount();
+		TemporalReference tref = TemporalReference::unreferenced();
+		for (int r=0;r<rasters;r++) {
+			auto raster = getRasterFromSource(r, rect, profiler);
+			Profiler::Profiler p("RASTER_METADATA_TO_POINTS_OPERATOR");
+			enhance(*points, *raster, names.at(r), profiler);
+			if (r == 0)
+				tref = raster->stref;
+			else
+				tref.intersect(raster->stref);
+		}
+		points->addDefaultTimestamps(tref.t1, tref.t2);
 	}
 	return points;
 }

c9cb1100f3acde33026335882e7869d1516c6273
koerberm <koerberm@mathematik.uni-marburg.de>
2015-05-30 02:18:27 +0200

    Added first implementation of the cache index-server. For now, the index always pipes requests directly to an available node-server. There is no index to lookup;

---
+++ mapping/cache/index/indexserver.cpp
@@ -0,0 +1,543 @@
+/*
+ * indexserver.cpp
+ *
+ *  Created on: 26.05.2015
+ *      Author: mika
+ */
+
+#include "cache/common.h"
+#include "cache/index/indexserver.h"
+
+#include <deque>
+#include <memory>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// socket() etc
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <unistd.h>
+#include <errno.h>
+#include <cstring>
+
+////////////////////////////////////////////////////////////
+//
+// JOB
+//
+////////////////////////////////////////////////////////////
+
+Job::Job(NP &node, CP &frontend_connection, CP &worker_connection) :
+		node(node), frontend_connection(std::move(frontend_connection)), worker_connection(
+				std::move(worker_connection)) {
+}
+
+////////////////////////////////////////////////////////////
+//
+// JOB-DEFINITION
+//
+////////////////////////////////////////////////////////////
+
+JobDefinition::JobDefinition(CP &frontend_connection, const std::string &graph_json,
+		const QueryRectangle& query) :
+		frontend_connection(std::move(frontend_connection)), query(query), graph_json(graph_json) {
+}
+
+// TODO: Why is there no implementation of the declared default constructor of QueryRectangle
+JobDefinition::JobDefinition(CP& frontend_connection) :
+		frontend_connection(std::move(frontend_connection)), query(0, 0, 0, 0, 0, 0, 0, EPSG_UNKNOWN) {
+}
+
+JobDefinition::~JobDefinition() {
+}
+
+class RasterJobDef: public JobDefinition {
+public:
+	RasterJobDef(const RasterJobDef&) = delete;
+	RasterJobDef operator=(const RasterJobDef&) = delete;
+	RasterJobDef(CP &frontend_connection);
+	RasterJobDef(CP &frontend_connection, const std::string &graph_json, const QueryRectangle& query,
+			GenericOperator::RasterQM query_mode);
+	virtual ~RasterJobDef();
+	virtual std::unique_ptr<Job> create_job(NP &node, CP worker_connection);
+	GenericOperator::RasterQM query_mode;
+};
+
+RasterJobDef::RasterJobDef(CP &frontend_connection, const std::string &graph_json,
+		const QueryRectangle& query, GenericOperator::RasterQM query_mode) :
+		JobDefinition(frontend_connection, graph_json, query), query_mode(query_mode) {
+}
+
+RasterJobDef::RasterJobDef(CP& frontend_connection) :
+		JobDefinition(frontend_connection) {
+	// Read the request directly from the connection
+	this->frontend_connection->stream->read(&graph_json);
+	QueryRectangle q(*this->frontend_connection->stream);
+	uint8_t qm;
+	this->frontend_connection->stream->read(&qm);
+	query = q;
+	query_mode = (qm == 1) ? GenericOperator::RasterQM::EXACT : GenericOperator::RasterQM::LOOSE;
+}
+
+std::unique_ptr<Job> RasterJobDef::create_job(NP &node, CP worker_connection) {
+	// Send request to worker
+	uint8_t cmd = Common::CMD_WORKER_GET_RASTER;
+	worker_connection->stream->write(cmd);
+	Common::writeRasterRequest(*worker_connection, graph_json, query, query_mode);
+
+	return std::unique_ptr<Job>(new Job(node, frontend_connection, worker_connection));
+}
+
+RasterJobDef::~RasterJobDef() {
+}
+
+////////////////////////////////////////////////////////////
+//
+// NODE
+//
+////////////////////////////////////////////////////////////
+
+void Node::add_worker(CP &con) {
+	workers.push_back(std::move(con));
+}
+
+Node::CP Node::get_worker() {
+	if (workers.empty())
+		throw NoSuchElementException("No worker available");
+	CP worker = std::move(workers.front());
+	workers.pop_front();
+	return worker;
+}
+
+bool Node::has_worker() {
+	return !workers.empty();
+}
+
+void Node::add_pending_job(JP& jd) {
+	pending_jobs.push_back(std::move(jd));
+}
+
+Node::JP Node::get_pending_job() {
+	if (pending_jobs.empty())
+		throw NoSuchElementException("No pending job available");
+	JP jd = std::move(pending_jobs.front());
+	pending_jobs.pop_front();
+	return jd;
+}
+
+bool Node::has_pending_job() {
+	return !pending_jobs.empty();
+}
+
+void Node::check_idle_workers(fd_set* readfds) {
+	auto wit = workers.begin();
+	while (wit != workers.end()) {
+		auto &wc = *wit;
+		if (FD_ISSET(wc->fd, readfds)) {
+			Log::error("Idle workers should never send date. Dropping worker.");
+			wit = workers.erase(wit);
+		}
+		else
+			++wit;
+	}
+}
+
+int Node::add_idle_workers(fd_set* readfds) {
+	int maxfd = -1;
+	for (auto &wc : workers) {
+		FD_SET(wc->fd, readfds);
+		maxfd = std::max(maxfd, wc->fd);
+	}
+	return maxfd;
+}
+
+////////////////////////////////////////////////////////////
+//
+// INDEX SERVER
+//
+////////////////////////////////////////////////////////////
+
+IndexServer::IndexServer(int frontend_port, int node_port) :
+		shutdown(false), frontend_port(frontend_port), node_port(node_port), next_node_id(1) {
+}
+
+IndexServer::~IndexServer() {
+}
+
+void IndexServer::run() {
+	int node_socket = Common::getListeningSocket(node_port);
+	int frontend_socket = Common::getListeningSocket(frontend_port);
+	Log::info("index-server: listening on node-port: %d and frontend-port: %d", node_port, frontend_port);
+
+	while (!shutdown) {
+		struct timeval tv { 2, 0 };
+		fd_set readfds;
+		FD_ZERO(&readfds);
+		// Add listen sockets
+		FD_SET(node_socket, &readfds);
+		FD_SET(frontend_socket, &readfds);
+
+		int maxfd = std::max(node_socket, frontend_socket);
+
+		// Add newly accepted connections
+		for (auto &fd : new_node_fds) {
+			FD_SET(fd, &readfds);
+			maxfd = std::max(maxfd, fd);
+		}
+
+		// Add control-connections and idle workers
+		for (auto &e : nodes) {
+			FD_SET(e.second->control_connection->fd, &readfds);
+			maxfd = std::max(maxfd, e.second->control_connection->fd);
+			maxfd = std::max(maxfd, e.second->add_idle_workers(&readfds));
+		}
+
+		// Add frontend-connections
+		for (auto &fc : frontend_connections) {
+			FD_SET(fc->fd, &readfds);
+			maxfd = std::max(maxfd, fc->fd);
+		}
+
+		// Add job-connections
+		for (auto &job : jobs) {
+			FD_SET(job->worker_connection->fd, &readfds);
+			maxfd = std::max(maxfd, job->worker_connection->fd);
+		}
+
+		int sel_ret = select(maxfd + 1, &readfds, nullptr, nullptr, &tv);
+		if (sel_ret < 0 && errno != EINTR) {
+			Log::error("Select returned error: %s", strerror(errno));
+		}
+		else if (sel_ret > 0) {
+			check_node_connections(&readfds);
+			check_node_handshake(&readfds, node_socket);
+
+			check_frontend_handshake(&readfds, frontend_socket);
+			check_frontend_connections(&readfds);
+
+			handle_jobs(&readfds);
+		}
+	}
+	close(node_socket);
+	close(frontend_socket);
+}
+
+void IndexServer::stop() {
+	Log::info("Shutting down.");
+	shutdown = true;
+}
+
+std::unique_ptr<std::thread> IndexServer::run_async() {
+	return std::unique_ptr<std::thread>(new std::thread(&IndexServer::run, this));
+}
+
+void IndexServer::check_node_connections(fd_set* readfds) {
+	auto nit = nodes.begin();
+	while (nit != nodes.end()) {
+		auto node = (*nit).second;
+		if (FD_ISSET(node->control_connection->fd, readfds)) {
+			try {
+				uint8_t cmd;
+				node->control_connection->stream->read(&cmd);
+				switch (cmd) {
+					default: {
+						std::ostringstream msg;
+						msg << "Received illegal command on control-connection for node: " <<  node->id;
+						throw NetworkException(msg.str());
+					}
+				}
+				// Check idle workers
+				node->check_idle_workers(readfds);
+				++nit;
+			} catch (NetworkException &ne) {
+				Log::error("Error on control-connection for node: %d. Dropping. Reason: %s", node->id,  ne.what());
+				// TODO: Redistribute queued jobs
+				nit = nodes.erase(nit);
+			}
+		}
+		else
+			++nit;
+	}
+}
+
+void IndexServer::check_node_handshake(fd_set* readfds, int node_socket) {
+	// Check new node connections
+	auto it = new_node_fds.begin();
+	while (it != new_node_fds.end()) {
+		auto &fd = *it;
+		if (FD_ISSET(fd, readfds)) {
+			try {
+				CP con(new SocketConnection(fd));
+				uint8_t cmd;
+				con->stream->read(&cmd);
+
+				switch (cmd) {
+					case Common::CMD_INDEX_NODE_HELLO: {
+						handleNodeHello(con);
+						break;
+					}
+					case Common::CMD_INDEX_REGISTER_WORKER: {
+						handleWorkerRegistration(con);
+						break;
+					}
+					default: {
+						std::ostringstream msg;
+						msg << "Received illegal command on node-connection with fd: " <<  fd;
+						throw NetworkException(msg.str());
+					}
+				}
+			} catch ( NetworkException &ne ) {
+				Log::error("Error on fresh node-connection. Dropping. Reason: %s", ne.what());
+			}
+			// remove from new fds
+			it = new_node_fds.erase(it);
+		}
+		else
+			++it;
+	}
+
+	// Accept new node connection
+	if (FD_ISSET(node_socket, readfds)) {
+		struct sockaddr_storage remote_addr;
+		socklen_t sin_size = sizeof(remote_addr);
+		int new_fd = accept(node_socket, (struct sockaddr *) &remote_addr, &sin_size);
+
+		if (new_fd == -1 && errno != EAGAIN && errno != EWOULDBLOCK) {
+			Log::error("Accept failed: %d", strerror(errno));
+		}
+		else if (new_fd > 0) {
+			Log::debug("New node-connection established on fd: %d", new_fd);
+			new_node_fds.push_back(new_fd);
+		}
+	}
+
+}
+
+void IndexServer::handleNodeHello(CP &connection) {
+	std::string host;
+	uint32_t port;
+	try {
+		connection->stream->read(&host);
+		connection->stream->read(&port);
+
+		uint32_t id = next_node_id++;
+
+		Log::info("Connection fd: %d", connection->fd);
+
+		NP node(new Node(id, host, port, connection));
+
+
+		Log::info("New node registered. ID: %d", id);
+		uint8_t code = Common::RESP_INDEX_NODE_HELLO;
+		Log::info("Node-Connection fd: %d", node->control_connection->fd);
+		node->control_connection->stream->write(code);
+		node->control_connection->stream->write(id);
+
+		nodes[id] = node;
+	} catch ( NetworkException &ne ) {
+		Log::error("Could not register new cache-node: %s", ne.what());
+	}
+}
+
+void IndexServer::handleWorkerRegistration(CP &connection) {
+	uint32_t id;
+	try {
+		connection->stream->read(&id);
+		auto n = nodes.at(id);
+		Log::info("New worker-connection for Node: %d", id);
+		// If we have pending jobs for this worker... assing it
+		if (n->has_pending_job())
+			jobs.push_back(n->get_pending_job()->create_job(n, std::move(connection)));
+		// Otherwise simply add this connection
+		else
+			n->add_worker(connection);
+
+	} catch (std::out_of_range &ore_) {
+		Log::error("Worker connection for unknown Node: %d. Discarding.", id);
+	} catch ( NetworkException &ne ) {
+		Log::error("Could not register new worker: %s", ne.what());
+	}
+}
+
+void IndexServer::check_frontend_handshake(fd_set* readfds, int frontend_socket) {
+	// Accept new node connection
+	if (FD_ISSET(frontend_socket, readfds)) {
+		struct sockaddr_storage remote_addr;
+		socklen_t sin_size = sizeof(remote_addr);
+		int new_fd = accept(frontend_socket, (struct sockaddr *) &remote_addr, &sin_size);
+
+		if (new_fd == -1 && errno != EAGAIN && errno != EWOULDBLOCK) {
+			Log::error("Accept failed: %d", strerror(errno));
+		}
+		else if (new_fd > 0) {
+			Log::debug("New front-connection established on fd: %d", new_fd);
+			frontend_connections.push_back(std::unique_ptr<SocketConnection>(new SocketConnection(new_fd)));
+		}
+	}
+}
+
+void IndexServer::check_frontend_connections(fd_set* readfds) {
+	auto it = frontend_connections.begin();
+	while (it != frontend_connections.end()) {
+		// Remove connection from idle frontend connections?
+		bool remove_connection = true;
+		auto &fc = *it;
+		if (FD_ISSET(fc->fd, readfds)) {
+			try {
+				uint8_t cmd;
+				if (fc->stream->read(&cmd, true)) {
+					switch (cmd) {
+						case Common::CMD_INDEX_GET_RASTER: {
+							std::unique_ptr<JobDefinition> jd(new RasterJobDef(fc));
+							try {
+								NP node = get_node_for_job(jd);
+								// We can process the job directly
+								if (node->has_worker())
+									jobs.push_back(jd->create_job(node, node->get_worker()));
+								// Add this job to the node's queue
+								else
+									node->add_pending_job(jd);
+							} catch (NoSuchElementException &nse) {
+								// TODO: This is ugly
+								fc = std::move(jd->frontend_connection);
+								std::string msg = "No worker-node available";
+								Log::error("No node registered for processing this request.");
+								uint8_t resp = Common::RESP_INDEX_ERROR;
+								fc->stream->write(resp);
+								fc->stream->write(msg);
+								remove_connection = false;
+							}
+
+							break;
+						}
+						default: {
+							Log::warn("Unknown command on frontend-connection: %d. Dropping connection.",
+									cmd);
+							break;
+						}
+					}
+				}
+				else {
+					Log::debug("Frontend-connection closed on fd: %d", fc->fd);
+				}
+			} catch (NetworkException &ne) {
+				Log::error("Error on frontend-connection with fd: %d. Dropping. Reason: %s", fc->fd,
+						ne.what());
+			}
+			if ( remove_connection )
+				it = frontend_connections.erase(it);
+			else
+				++it;
+		}
+		else
+			++it;
+	}
+}
+
+void IndexServer::handle_jobs(fd_set* readfds) {
+	// Holds all new jobs
+	std::vector<std::unique_ptr<Job>> new_jobs;
+
+	auto it = jobs.begin();
+	while (it != jobs.end()) {
+		auto &job = (*it);
+		auto &wc = job->worker_connection;
+		auto &fc = job->frontend_connection;
+		if (FD_ISSET(wc->fd, readfds)) {
+			// Job done?
+			bool done = true;
+			// May we reuse the connections
+			bool reuse_worker = true;
+			bool reuse_frontend = true;
+
+			uint8_t resp;
+
+			try {
+				wc->stream->read(&resp);
+				switch (resp) {
+					case Common::RESP_WORKER_RESULT_READY: {
+						// Read delivery id
+						uint64_t delivery_id;
+						wc->stream->read(&delivery_id);
+						Log::debug("Worker returned result. Delivery-ID: %d", delivery_id);
+
+						// Send delivery-id and node address
+						uint8_t f_resp = Common::RESP_INDEX_GET;
+						fc->stream->write(f_resp);
+						fc->stream->write(job->node->host);
+						fc->stream->write(job->node->port);
+						fc->stream->write(delivery_id);
+						Log::debug("Worker finished processing request.");
+						break;
+					}
+					case Common::RESP_WORKER_NEW_CACHE_ENTRY: {
+						Log::info("Worker returned new result to cache.");
+						done = false;
+						break;
+					}
+					case Common::RESP_WORKER_ERROR: {
+						std::string msg;
+						wc->stream->read(&msg);
+						Log::warn("Worker returned error %s", msg.c_str());
+						uint8_t f_resp = Common::RESP_INDEX_ERROR;
+						fc->stream->write(f_resp);
+						fc->stream->write(msg);
+						break;
+					}
+					default: {
+						Log::error("Worker returned unknown code: %d. Terminating worker-connection.", resp);
+						uint8_t f_resp = Common::RESP_INDEX_ERROR;
+						std::string msg = "Internal error";
+						fc->stream->write(f_resp);
+						fc->stream->write(msg);
+						reuse_worker = false;
+						break;
+					}
+				}
+			} catch (NetworkException &ne) {
+				reuse_worker = false;
+				reuse_frontend = false;
+				Log::error("Error while processing job. Both connections are dropped.");
+			}
+			// Job done
+			if (done) {
+				// Release frontend connection
+				if (reuse_frontend) {
+					Log::debug("Releasing frontend-connection.");
+					frontend_connections.push_back(std::move(fc));
+				}
+
+				// Schedule next if available
+				if (reuse_worker && job->node->has_pending_job()) {
+					Log::debug("Directly reusing worker-connection for queued job.");
+					new_jobs.push_back(job->node->get_pending_job()->create_job(job->node, std::move(wc)));
+				}
+				else if (reuse_worker) {
+					Log::debug("Releasing worker-connection.");
+					job->node->add_worker(wc);
+				}
+
+				it = jobs.erase(it);
+			}
+			else
+				++it;
+		}
+		// FD not set
+		else
+			++it;
+	}
+	for ( auto &j : new_jobs) {
+		jobs.push_back( std::move(j) );
+	}
+}
+
+IndexServer::NP IndexServer::get_node_for_job(const std::unique_ptr<JobDefinition> &job_def) {
+	// TODO: BIG TODO
+	(void) job_def;
+	if (nodes.empty())
+		throw NoSuchElementException("No nodes available");
+	else
+		return nodes.begin()->second;
+}
---
+++ mapping/cache/index/indexserver.h
@@ -0,0 +1,217 @@
+/*
+ * indexserver.h
+ *
+ *  Created on: 26.05.2015
+ *      Author: mika
+ */
+
+#ifndef INDEX_INDEXSERVER_H_
+#define INDEX_INDEXSERVER_H_
+
+#include "cache/common.h"
+#include "util/log.h"
+
+#include <string>
+#include <thread>
+#include <map>
+#include <vector>
+#include <deque>
+
+class Job;
+class Node;
+class IndexServer;
+
+//
+// Describes a currently processed job.
+// Basically holds the to participating connections
+// and a reference to the node.
+// When a job is created, the request is completely
+// read from the frontend and passed to the worker.
+// We only read responses from the worker within
+// a job.
+//
+class Job {
+	typedef std::unique_ptr<SocketConnection> CP;
+	typedef std::shared_ptr<Node> NP;
+public:
+	Job( NP &node, CP &frontend_connection, CP &worker_connection );
+	Job( const Job& ) = delete;
+	Job operator=( const Job& ) = delete;
+	// The node, the worker-connection belongs to
+	NP node;
+	// The connection to deliver results to
+	CP frontend_connection;
+	// The used worker connection
+	CP worker_connection;
+};
+
+//
+// Definition of a scheduled job.
+// Implementations must provide a method
+// to transform the definition into a job
+// Means: Send a request to the worker
+//
+
+class JobDefinition {
+protected:
+	typedef std::unique_ptr<SocketConnection> CP;
+	typedef std::shared_ptr<Node> NP;
+	JobDefinition(CP &frontend_connection);
+	JobDefinition(CP &frontend_connection, const std::string &graph_json, const QueryRectangle &query );
+public:
+	virtual ~JobDefinition();
+	virtual std::unique_ptr<Job> create_job( NP &node, CP worker_connection ) = 0;
+	// The connection which issued this job
+	CP frontend_connection;
+	// Common parts of a query
+	QueryRectangle query;
+	std::string graph_json;
+};
+
+
+//
+// Represents a cache-node.
+// Each node has a control connection and
+// up to n workers.
+// A worker may be obtained by the get_worker method.
+// The caller then owns the worker connection and is responsible
+// for dropping or returning it via add_worker.
+// Same is with job-definitions. They should only be enqueued here
+// if there is noch worker available on this node.
+// The server must take care, that enqueued jobs are processed asap.
+//
+//
+
+class Node {
+	typedef std::unique_ptr<SocketConnection> CP;
+	typedef std::unique_ptr<JobDefinition> JP;
+public:
+	Node( const Node& ) = delete;
+	Node operator=( const Node& ) = delete;
+	Node(uint32_t id, std::string host, int port, CP &control_connection) :
+			id(id), host(host), port(port), control_connection(std::move(control_connection)) {
+	}
+	;
+	// Adds an idle worker
+	void add_worker(CP &con);
+	// Returns an idle worker -> Must be returned or dropped
+	CP get_worker();
+	// Tells wheter this node has idle workers
+	bool has_worker();
+
+	// Adds a job to schedule (only to be called if there is no worker available)
+	void add_pending_job(JP &jd);
+	// Retrieves a pending job from the queue
+	JP get_pending_job();
+	// Tells wheter this node has pending jobs
+	bool has_pending_job();
+
+	// Adds the FDs of currently idle workers to the given fd_set
+	// and returns the max-fd among them
+	int add_idle_workers( fd_set *readfds );
+
+	// Checks the fds of all idle workers for available data.
+	// Since this should not happen, all workers with data are dropped
+	void check_idle_workers( fd_set *readfds );
+
+	// The unique id of this node
+	const uint32_t id;
+	// The hostname of this node
+	const std::string host;
+	// The port for delivery connections on this node
+	const uint32_t port;
+	// The control-connection
+	CP control_connection;
+private:
+	// All idle workers
+	std::deque<CP> workers;
+	// All jobs to be scheduledd
+	std::deque<JP> pending_jobs;
+};
+
+//
+// The heart of the cache. The index
+// accepts connections from the client as well
+// as from cache-nodes.
+// Cache-nodes have to establish a so called control-connection
+// on which they send their hostname and delivery port. After
+// doing so, a unique id is assigned to the node. All workers of
+// this node must use this id to register themselves at the index.
+//
+// Client-connections may issue requests to the server.
+// The server handles everything in a single thread.
+//
+
+
+class IndexServer {
+	typedef std::unique_ptr<SocketConnection> CP;
+	typedef std::shared_ptr<Node> NP;
+public:
+	IndexServer( int frontend_port, int node_port );
+	virtual ~IndexServer();
+	// Fires up the index-server and will return after
+	// stop() is invoked by another thread
+	void run();
+	// Fires up the index-server in a separate thread
+	// and returns it.
+	std::unique_ptr<std::thread> run_async();
+	// Triggers the shutdown of the index-server
+	// Subsequent calls to run or run_async have undefined
+	// behaviour
+	virtual void stop();
+
+private:
+	// Checks if there are new connections from the frontend
+	void check_frontend_handshake( fd_set *readfds, int frontend_socket );
+
+	// Checks idle frontend-connections for data
+	// and creates the corresponding jobs on requests
+	void check_frontend_connections( fd_set *readfds );
+
+	// Checks if there are new connections from node-servers.
+	// Also handles hello requests from nodes and workers.
+	void check_node_handshake( fd_set *readfds, int node_socket );
+
+	// Checks a node's control-connection as well
+	// as its idle worker-connections for errors resp. commands.
+	void check_node_connections( fd_set *readfds );
+
+	// Processes the currently running jobs
+	void handle_jobs( fd_set *readfds );
+
+	// Processes a hello from a cache-node
+	// and registers the node
+	void handleNodeHello( CP &connection );
+
+	// Processes a hello from a node's worker-thread
+	// and attaches the resulting connection to the
+	// corresponding node-instance
+	void handleWorkerRegistration( CP &connection );
+
+	// BIG TODO
+	// Fetches the right node for handling the given job
+	// Desired behaviour:
+	// If response is cached: Choose node which holds it
+	// else: Round-robbin or sth.
+	NP get_node_for_job(const std::unique_ptr<JobDefinition> &job_def);
+
+	// Indicator telling if the server should shutdown
+	bool shutdown;
+	// The port to accept client connections on
+	int frontend_port;
+	// The port to accept connections from cache-nodes on
+	int node_port;
+	// The currently known nodes
+	std::map<uint32_t,NP> nodes;
+	// The currently idle frontend-connections
+	std::vector<CP> frontend_connections;
+	// The currently scheduled jobs
+	std::vector<std::unique_ptr<Job>> jobs;
+	// Holds fds of newly accepted connections from workers
+	std::vector<int> new_node_fds;
+	// The next id to assign to a node
+	uint32_t next_node_id;
+
+};
+
+#endif /* INDEX_INDEXSERVER_H_ */

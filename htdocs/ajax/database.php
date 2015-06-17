4e9665958af798cc20cb4918d9a73d7a8f58921b
Christian Authmann <authmanc@mathematik.uni-marburg.de>
2015-06-17 13:58:15 +0200

    revert accidental change of db credentials

--- htdocs/ajax/database.php
+++ htdocs/ajax/database.php
@@ -11,7 +11,8 @@ class DB {
 
 		// dbname = 'idessa' user = 'idessa' password = 'idessa'
 		// pgsql:host=localhost;port=5432;dbname=testdb;user=bruce;password=mypass
-		self::$db = new PDO('pgsql:host=localhost;port=5432;dbname=idessa', 'idessa', 'idessa');
+		//self::$db = new PDO('pgsql:host=localhost;port=5432;dbname=idessa', 'idessa', 'idessa');
+		self::$db = new PDO('pgsql:host=***REMOVED***;dbname=webusers', 'webusers', 'idessa');
 		self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		self::$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 		self::$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); // TODO: check FETCH_CLASS!
@@ -36,15 +37,15 @@ class DB {
 		$stmt->execute($params);
 		return $stmt->rowCount();
 	}
-	
+
 	static public function beginTransaction() {
 		self::$db->beginTransaction();
 	}
-	
+
 	static public function commit() {
 		self::$db->commit();
 	}
-	
+
 	static public function getLastInsertedId($sequenceName) {
 		return self::$db->lastInsertId($sequenceName);
 	}

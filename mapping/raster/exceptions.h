04f8224b6560bbbe36153bdb1d845fab04c7b17a
Christian Authmann <authmanc@mathematik.uni-marburg.de>
2015-06-29 15:25:40 +0200

    Exceptions: use more magic to allow an exception hierarchy

--- mapping/raster/exceptions.h
+++ mapping/raster/exceptions.h
@@ -3,20 +3,27 @@
 
 #include "util/concat.h"
 
-#include <string>
-#include <exception>
 #include <stdexcept>
 
 
-#define _CUSTOM_EXCEPTION_CLASS(C) class C : public std::runtime_error { public: C(const std::string &msg) : std::runtime_error(std::string(#C) + ": " + msg) {}}
+#define _CUSTOM_EXCEPTION_CLASS_PARENT(C, PARENT) class C : public PARENT { public: C(const std::string &msg) : PARENT(#C ": " + msg) {}}
+
+// This is just some magic to get around the fact that macros cannot have default parameters
+#define _CUSTOM_EXCEPTION_CLASS_DEFAULT(C) _CUSTOM_EXCEPTION_CLASS_PARENT(C, std::runtime_error)
+
+#define _CUSTOM_EXCEPTION_CLASS_GET_3RD_PARAM(p1, p2, p3, ...) p3
+#define _CUSTOM_EXCEPTION_CLASS_CHOOSER(...) _CUSTOM_EXCEPTION_CLASS_GET_3RD_PARAM(__VA_ARGS__, _CUSTOM_EXCEPTION_CLASS_PARENT, _CUSTOM_EXCEPTION_CLASS_DEFAULT, 0)
+
+#define _CUSTOM_EXCEPTION_CLASS(...) _CUSTOM_EXCEPTION_CLASS_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
+
 
 _CUSTOM_EXCEPTION_CLASS(ArgumentException);
 _CUSTOM_EXCEPTION_CLASS(ImporterException);
 _CUSTOM_EXCEPTION_CLASS(ExporterException);
-_CUSTOM_EXCEPTION_CLASS(SourceException);
 _CUSTOM_EXCEPTION_CLASS(MetadataException);
 _CUSTOM_EXCEPTION_CLASS(ConverterException);
 _CUSTOM_EXCEPTION_CLASS(OperatorException);
+_CUSTOM_EXCEPTION_CLASS(SourceException, OperatorException);
 _CUSTOM_EXCEPTION_CLASS(OpenCLException);
 _CUSTOM_EXCEPTION_CLASS(PlatformException);
 _CUSTOM_EXCEPTION_CLASS(cURLException);
@@ -24,16 +31,22 @@ _CUSTOM_EXCEPTION_CLASS(SQLiteException);
 _CUSTOM_EXCEPTION_CLASS(GDALException);
 _CUSTOM_EXCEPTION_CLASS(NetworkException);
 _CUSTOM_EXCEPTION_CLASS(FeatureException);
+
 // Added Micha
-_CUSTOM_EXCEPTION_CLASS(NoSuchElementException);
-_CUSTOM_EXCEPTION_CLASS(NotInitializedException);
-_CUSTOM_EXCEPTION_CLASS(TimeoutException);
-_CUSTOM_EXCEPTION_CLASS(InterruptedException);
-_CUSTOM_EXCEPTION_CLASS(DeliveryException);
-_CUSTOM_EXCEPTION_CLASS(IllegalStateException);
+_CUSTOM_EXCEPTION_CLASS(CacheException);
+_CUSTOM_EXCEPTION_CLASS(NoSuchElementException, CacheException);
+_CUSTOM_EXCEPTION_CLASS(NotInitializedException, CacheException);
+_CUSTOM_EXCEPTION_CLASS(TimeoutException, CacheException);
+_CUSTOM_EXCEPTION_CLASS(InterruptedException, CacheException);
+_CUSTOM_EXCEPTION_CLASS(DeliveryException, CacheException);
+_CUSTOM_EXCEPTION_CLASS(IllegalStateException, CacheException);
 
 
 
 #undef _CUSTOM_EXCEPTION_CLASS
+#undef _CUSTOM_EXCEPTION_CLASS_CHOOSER
+#undef _CUSTOM_EXCEPTION_CLASS_GET_3RD_PARAM
+#undef _CUSTOM_EXCEPTION_CLASS_DEFAULT
+#undef _CUSTOM_EXCEPTION_CLASS_PARENT
 
 #endif

dc4cdb2765df405f73d84722de580240018c4587
Christian Authmann <authmanc@mathematik.uni-marburg.de>
2015-07-17 15:05:55 +0200

    points2raster: ensure proper passing of QueryResolution. Deal with collections containing multi-point Features.

--- mapping/operators/combined/points2raster.cpp
+++ mapping/operators/combined/points2raster.cpp
@@ -55,28 +55,29 @@ std::unique_ptr<GenericRaster> PointsToRasterOperator::getRaster(const QueryRect
 
 	RasterOpenCL::init();
 
-	QueryRectangle rect2 = rect;
-	rect2.enlarge(radius);
-	auto points = getPointCollectionFromSource(0, rect2, profiler);
-	//TODO: ensure that points are single points?!
+	QueryRectangle rect_larger = rect;
+	rect_larger.enlarge(radius);
+
+	QueryRectangle rect_points(rect_larger, rect_larger, QueryResolution::none());
+	auto points = getPointCollectionFromSource(0, rect_points, profiler);
 
 	if (renderattribute == "") {
 		DataDescription dd_acc(GDT_UInt16, 0, 65535, true, 0);
-		auto accumulator = GenericRaster::create(dd_acc, rect2, rect2.xres, rect2.yres, 0, GenericRaster::Representation::CPU);
+		auto accumulator = GenericRaster::create(dd_acc, rect_larger, rect_larger.xres, rect_larger.yres, 0, GenericRaster::Representation::CPU);
 		Raster2D<uint16_t> *acc = (Raster2D<uint16_t> *) accumulator.get();
 		acc->clear(0);
 
-		for (Coordinate &p : points->coordinates) {
-			double x = p.x, y = p.y;
+		for (auto feature : *points) {
+			for (auto &p : feature) {
+				auto px = acc->WorldToPixelX(p.x);
+				auto py = acc->WorldToPixelY(p.y);
+				if (px < 0 || py < 0 || px >= acc->width || py >= acc->height)
+					continue;
 
-			auto px = acc->WorldToPixelX(x);
-			auto py = acc->WorldToPixelY(y);
-			if (px < 0 || py < 0 || px >= acc->width || py >= acc->height)
-				continue;
-
-			uint32_t val = acc->get(px, py);
-			val = std::min((uint32_t) acc->dd.max, val+1);
-			acc->set(px, py, val);
+				uint32_t val = acc->get(px, py);
+				val = std::min((uint32_t) acc->dd.max, val+1);
+				acc->set(px, py, val);
+			}
 		}
 
 		DataDescription dd_blur(GDT_Byte, 0, 255, true, 0);
@@ -96,36 +97,33 @@ std::unique_ptr<GenericRaster> PointsToRasterOperator::getRaster(const QueryRect
 		const float MIN = 0, MAX = 10000;
 		DataDescription dd_sum(GDT_Float32, MIN, MAX, true, 0);
 		DataDescription dd_count(GDT_UInt16, 0, 65534, true, 0);
-		auto r_sum = GenericRaster::create(dd_sum, rect2, rect2.xres, rect2.yres, 0, GenericRaster::Representation::CPU);
-		auto r_count = GenericRaster::create(dd_count, rect2, rect2.xres, rect2.yres, 0, GenericRaster::Representation::CPU);
+		auto r_sum = GenericRaster::create(dd_sum, rect_larger, rect_larger.xres, rect_larger.yres, 0, GenericRaster::Representation::CPU);
+		auto r_count = GenericRaster::create(dd_count, rect_larger, rect_larger.xres, rect_larger.yres, 0, GenericRaster::Representation::CPU);
 		Raster2D<float> *sum = (Raster2D<float> *) r_sum.get();
 		sum->clear(0);
 		Raster2D<uint16_t> *count = (Raster2D<uint16_t> *) r_count.get();
 		count->clear(0);
 
 		auto &vec = points->local_md_value.getVector(renderattribute);
-		int i=0;
-		for (Coordinate &p : points->coordinates) {
-			double x = p.x, y = p.y;
+		for (auto feature : *points) {
+			for (auto &p : feature) {
+				auto px = sum->WorldToPixelX(p.x);
+				auto py = sum->WorldToPixelY(p.y);
+				if (px < 0 || py < 0 || px >= sum->width || py >= sum->height)
+					continue;
 
-			auto px = sum->WorldToPixelX(x);
-			auto py = sum->WorldToPixelY(y);
-			if (px < 0 || py < 0 || px >= sum->width || py >= sum->height)
-				continue;
+				auto attr = vec[feature];
+				if (std::isnan(attr))
+					continue;
 
-			auto attr = vec[i];
-			if (std::isnan(attr))
-				continue;
+				auto sval = sum->get(px, py);
+				sval = sval+attr;
+				sum->set(px, py, sval);
 
-			auto sval = sum->get(px, py);
-			sval = sval+attr;
-			sum->set(px, py, sval);
-
-			auto cval = count->get(px, py);
-			cval = std::min((decltype(cval+1)) count->dd.max, cval+1);
-			count->set(px, py, cval);
-
-			i++;
+				auto cval = count->get(px, py);
+				cval = std::min((decltype(cval+1)) count->dd.max, cval+1);
+				count->set(px, py, cval);
+			}
 		}
 
 		DataDescription dd_blur(GDT_Float32, MIN, MAX, true, 0);

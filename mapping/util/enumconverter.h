c5491bfa3a20f29991d0a4ce4bfd80ccd9ea0755
Christian Authmann <authmanc@mathematik.uni-marburg.de>
2015-11-03 11:54:32 +0100

    enumconverter.h: add a little helper to convert enums into strings and back CSVPointSource: remove autodetection for columns, instead have the user specify all the columns and their types as parameters. Adjusted testcases to the new CSVPointSource and made sure they still pass.

---
+++ mapping/util/enumconverter.h
@@ -0,0 +1,45 @@
+/*
+ * Templates for mapping enums to strings and back.
+ *
+ * Mostly used for parameter parsing. Don't use it for enums with lots of values.
+ *
+ * For an example how to use this, see operators/points/csvpointsource.cpp
+ */
+
+#include "util/exceptions.h"
+
+#include <string>
+#include <tuple>
+#include <vector>
+#include <json/json.h>
+
+
+template<typename T, const std::vector< std::pair<T, std::string> > &map>
+struct EnumConverter {
+	EnumConverter() = delete;
+
+	static const std::string &to_string(T t) {
+		for (auto &tuple : map) {
+			if (tuple.first == t)
+				return tuple.second;
+		}
+		throw ArgumentException("No string found for enum value");
+	}
+
+	static const std::string &default_string() {
+		return map.at(0).second;
+	}
+
+	static T from_string(const std::string &s) {
+		for (auto &tuple : map) {
+			if (tuple.second == s)
+				return tuple.first;
+		}
+		throw ArgumentException(concat("No enum value found for identifier \"", s, "\""));
+	}
+
+	static T from_json(const Json::Value &root, const std::string &name) {
+		auto str = root.get(name, default_string()).asString();
+		return from_string(str);
+	}
+};

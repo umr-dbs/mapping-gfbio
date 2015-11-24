fd00d63fd79706a9f37315c7726ac70d00695fd3
mattig <mattig@mathematik.uni-marburg.de>
2015-11-24 10:24:18 +0100

    removed TimeFormat class, moved Format enum into TimeParser
    moved TimeParser subclasses into .cpp file
    
    renamed factory method
    
    fixed make_unique creation of parsers
    
    added timetype attribute to timeparser
    
    added custom exception for timeparser
    
    adjusted tests to changes

--- mapping/test/unittests/temporal/timeparser.cpp
+++ mapping/test/unittests/temporal/timeparser.cpp
@@ -1,28 +1,38 @@
 #include <gtest/gtest.h>
 
 #include "util/timeparser.h"
+#include "util/exceptions.h"
 
 
 TEST(TimeParser, testSeconds){
-	auto parser = TimeParser::getTimeParser(TimeFormat::getSecondsFormat());
+	auto parser = TimeParser::create(TimeParser::Format::SECONDS);
 
 	EXPECT_FLOAT_EQ(1447240271, parser->parse("1447240271"));
+
+	EXPECT_THROW(parser->parse("TEXT"), TimeParseException);
+	EXPECT_THROW(parser->parse("999999999999999999999"), TimeParseException);
 }
 
 TEST(TimeParser, testDMHYM){
-	auto parser = TimeParser::getTimeParser(TimeFormat::getDMYHMFormat());
+	auto parser = TimeParser::create(TimeParser::Format::DMYHM);
 
 	EXPECT_FLOAT_EQ(1447240260, parser->parse("11-Nov-2015  11:11"));
+
+	EXPECT_THROW(parser->parse("32-Nov-2015  11:11"), TimeParseException);
 }
 
 TEST(TimeParser, testISO){
-	auto parser = TimeParser::getTimeParser(TimeFormat::getISOFormat());
+	auto parser = TimeParser::create(TimeParser::Format::ISO);
 
 	EXPECT_FLOAT_EQ(1447240271, parser->parse("2015-11-11T11:11:11"));
+
+	EXPECT_THROW(parser->parse("2015-11-32T11:11:11"), TimeParseException);
 }
 
 TEST(TimeParser, testCustom){
-	auto parser = TimeParser::getTimeParser(TimeFormat::getCustomFormat("%d.%m.%y %H:%M"));
+	auto parser = TimeParser::createCustom("%d.%m.%y %H:%M");
 
 	EXPECT_FLOAT_EQ(1447240260, parser->parse("11.11.15 11:11"));
+
+	EXPECT_THROW(parser->parse("32.11.15 11:11"), TimeParseException);
 }
--- mapping/util/timeparser.h
+++ mapping/util/timeparser.h
@@ -1,81 +1,9 @@
 #ifndef UTIL_TIMEPARSER_H_
 #define UTIL_TIMEPARSER_H_
 
-#include "util/exceptions.h"
 #include <ctime>
 #include "util/make_unique.h"
-
-/**
- * This class represents different TimeFormats and indicates the format
- * in which a date/time is represented as a String
- */
-class TimeFormat {
-public:
-	enum class Format {
-		SECONDS,  // unix timestamp as seconds
-		DMYHM,   // %d-%B-%Y  %H:%M
-		ISO,    // ISO 8601 format
-		CUSTOM // custom format specified in field custom_format as strptime compatible string
-	};
-
-	/**
-	 * get format for unix time in seconds
-	 * @return format for unix time in seconds
-	 */
-	static TimeFormat getSecondsFormat(){
-		return TimeFormat(Format::SECONDS);
-	}
-
-	/**
-	 * get format for %d-%B-%Y  %H:%M
-	 * @return format for %d-%B-%Y  %H:%M
-	 */
-	static TimeFormat getDMYHMFormat(){
-		return TimeFormat(Format::DMYHM);
-	}
-
-	/**
-	 * get format for ISO8601
-	 * @return format for ISO 8601
-	 */
-	static TimeFormat getISOFormat(){
-		return TimeFormat(Format::ISO);
-	}
-
-	/**
-	 * get format for custom format
-	 * @param custom_format strptime compatible custom format
-	 * @return format for custom format
-	 */
-	static TimeFormat getCustomFormat(const std::string& custom_format){
-		return TimeFormat(Format::CUSTOM, custom_format);
-	}
-
-	/**
-	 * get format by name
-	 * @param formatName the name of the format
-	 * @return format for the given name
-	 */
-	static TimeFormat getFormatByName(const std::string& formatName){
-		if(formatName == "seconds")
-			return TimeFormat(Format::SECONDS);
-		if(formatName == "dmyhm")
-			return TimeFormat(Format::DMYHM);
-		if(formatName =="iso")
-			return TimeFormat(Format::ISO);
-
-		throw ArgumentException("Invalid format for time");
-	}
-
-	Format format;
-	std::string custom_format;
-
-private:
-	TimeFormat(Format format, std::string custom_format) : format(format), custom_format(custom_format){};
-
-	TimeFormat(Format format) : format(format){};
-
-};
+#include "datatypes/spatiotemporal.h"
 
 /**
  * This class provides methods to parse dates and datetimes from different
@@ -84,12 +12,34 @@ private:
 class TimeParser {
 public:
 
+	enum class Format {
+		SECONDS,  // unix timestamp as seconds
+		DMYHM,   // %d-%B-%Y  %H:%M
+		ISO,    // ISO 8601 format
+		CUSTOM // custom format specified in field custom_format as strptime compatible string
+	};
+
 	/**
 	 * Creates a parser for the given time format
 	 * @param timeFormat the format for the parser
 	 * @return a parser for the given time format
 	 */
-	static std::unique_ptr<TimeParser> getTimeParser(const TimeFormat timeFormat);
+	static std::unique_ptr<TimeParser> create(const Format timeFormat);
+
+	/**
+	 * Creates a parser for a custom time format
+	 * @param customFormat the strptime compatible format for the parser
+	 * @return a parser for the given custom time format
+	 */
+	static std::unique_ptr<TimeParser> createCustom(const std::string& customFormat);
+
+	/**
+	 * Get the timetype for the resulting timestamps
+	 * @return the timetype for the resulting timestamps
+	 */
+	timetype_t getTimeType(){
+		return timeType;
+	}
 
 	/**
 	 * parse the given time string and return a corresponding value
@@ -101,70 +51,10 @@ public:
 	virtual ~TimeParser() = default;
 
 protected:
-	TimeParser() = default;
+	TimeParser(const timetype_t timeType);
 
+	timetype_t timeType;
 };
-
-/**
- * Parser for UNIX timestamps in seconds
- */
-class TimeParserSeconds : public TimeParser {
-public:
-
-	virtual double parse(const std::string& timeString) const{
-		return stod(timeString);
-	}
-};
-
-/**
- * Parser for time as "%d-%B-%Y  %H:%M"
- */
-class TimeParserDMYHM : public TimeParser {
-public:
-
-	virtual double parse(const std::string& timeString) const{
-		std::tm tm;
-		if (strptime(timeString.c_str(), "%d-%B-%Y  %H:%M", &tm))
-			return timegm(&tm);
-		throw ConverterException("Could not parse time string");
-	}
-};
-
-/**
- * Parser for Strings in ISO8601 format
- */
-class TimeParserISO : public TimeParser {
-public:
-
-	virtual double parse(const std::string& timeString) const{
-		//TODO: support entirety of ISO8601 combatible formats https://en.wikipedia.org/wiki/ISO_8601
-		std::tm tm;
-		if (strptime(timeString.c_str(), "%Y-%m-%dT%H:%M:%S", &tm))
-			return timegm(&tm);
-		throw ConverterException("Could not parse time string");
-	}
-};
-
-/**
- * Parser for time in a custom format that is strptime compatible
- */
-class TimeParserCustom : public TimeParser {
-public:
-
-	TimeParserCustom(const std::string format) : format(format){};
-
-	virtual double parse(const std::string& timeString) const{
-		std::tm tm;
-		if (strptime(timeString.c_str(), format.c_str(), &tm))
-			return timegm(&tm);
-		throw ConverterException("Could not parse time string");
-	}
-
-private:
-	std::string format;
-
-};
-
 
 
 #endif

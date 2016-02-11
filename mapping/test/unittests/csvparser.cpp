341389126527990149f9af551e85f544835908ec
mattig <mattig@mathematik.uni-marburg.de>
2016-02-11 12:24:57 +0100

    optimize code cf. rVAT433128e27dfb

--- mapping/test/unittests/csvparser.cpp
+++ mapping/test/unittests/csvparser.cpp
@@ -1,9 +1,10 @@
+#include "util/csvparser.h"
+
 #include <gtest/gtest.h>
 #include <string>
 #include <sstream>
-#include "util/csvparser.h"
 
-void toCSV(std::stringstream& ss, const std::vector<std::vector<std::string>>& result, const std::string& delim, const std::string& endl) {
+static void toCSV(std::stringstream& ss, const std::vector<std::vector<std::string>>& result, const std::string& delim, const std::string& endl) {
 	for(auto fields : result ){
 		ss << fields[0];
 		for(size_t i = 1; i < fields.size(); ++i){
@@ -13,27 +14,22 @@ void toCSV(std::stringstream& ss, const std::vector<std::vector<std::string>>& r
 	}
 }
 
-std::vector<std::vector<std::string>> simple(){
-	std::vector<std::vector<std::string>> result = {{"a", "b", "c"}, {"testa1", "testb1", "testc1"}, {"d!\u00A7\00FC %&/()", "", "f"}};
-	return result;
-}
+static const std::vector<std::vector<std::string>> simple({{"a", "b", "c"}, {"testa1", "testb1", "testc1"}, {"d!\u00A7\00FC %&/()", "", "f"}});
 
-std::vector<std::vector<std::string>> quotes(){
-	std::vector<std::vector<std::string>> result = {{"a", "b", "c"}, {"\"testa1\"", "\"testb \"\"1\"\"\"", "testc1"}, {"d!\u00A7\00FC %&/()", "", "f"}};
-	return result;
-}
+static const std::vector<std::vector<std::string>> quotes({{"a", "b", "c"}, {"\"testa1\"", "\"testb \"\"1\"\"\"", "testc1"}, {"d!\u00A7\00FC %&/()", "", "f"}});
 
-std::vector<std::vector<std::string>> lineBreaksInQuotes(const std::string& endl){
+
+static std::vector<std::vector<std::string>> lineBreaksInQuotes(const std::string& endl){
 	std::vector<std::vector<std::string>> result = {{"a", "b", "c"}, {"\"test" + endl + "a1\"", "\"testb" + endl + endl + "\"\"1\"\"" + endl + "\"", "testc1"}, {"d!\u00A7\00FC %&/()", "", "f"}};
 	return result;
 }
 
-std::vector<std::vector<std::string>> delimInQuotes(const std::string& delim){
+static std::vector<std::vector<std::string>> delimInQuotes(const std::string& delim){
 	std::vector<std::vector<std::string>> result = {{"a", "b", "c"}, {"\"test" + delim + "a1\"", "\"testb" + delim + delim + "\"\"1\"\"" + delim + "\"", "testc1"}, {"d", "e", "f"}};
 	return result;
 }
 
-void checkParseResult(CSVParser& parser, const std::vector<std::vector<std::string>> expected) {
+static void checkParseResult(CSVParser& parser, const std::vector<std::vector<std::string>> expected) {
 	for(std::vector<std::string> fields : expected) {
 		auto tuple = parser.readTuple();
 		ASSERT_EQ(fields.size(), tuple.size());
@@ -48,60 +44,60 @@ void checkParseResult(CSVParser& parser, const std::vector<std::vector<std::stri
 TEST(CSVParser, simpleComma) {
 	std::string delim = ",";
 	std::string endl = "\n";
-	auto input = simple();
+	auto& input = simple;
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, delim.at(0), '\n');
+	CSVParser parser(ss, delim.at(0));
 	checkParseResult(parser, input);
 }
 
 TEST(CSVParser, simpleSemicolon) {
 	std::string delim = ";";
 	std::string endl = "\n";
-	auto input = simple();
+	auto& input = simple;
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, delim.at(0), '\n');
+	CSVParser parser(ss, delim.at(0));
 	checkParseResult(parser, input);
 }
 
 TEST(CSVParser, simpleCommaCRLF) {
 	std::string delim = ",";
 	std::string endl = "\r\n";
-	auto input = simple();
+	auto& input = simple;
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, delim.at(0), '\n');
+	CSVParser parser(ss, delim.at(0));
 	checkParseResult(parser, input);
 }
 
 TEST(CSVParser, simpleSemicolonCRLF) {
 	std::string delim = ";";
 	std::string endl = "\r\n";
-	auto input = simple();
+	auto& input = simple;
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, delim.at(0), '\n');
+	CSVParser parser(ss, delim.at(0));
 	checkParseResult(parser, input);
 }
 
 TEST(CSVParser, DISABLED_simpleWrongDelim) {
 	std::string delim = ";";
 	std::string endl = "\n";
-	auto input = simple();
+	auto& input = simple;
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, ',', '\n');
+	CSVParser parser(ss, ',');
 	checkParseResult(parser, input);
 }
 
 TEST(CSVParser, quotes) {
 	std::string delim = ",";
 	std::string endl = "\n";
-	auto input = quotes();
+	auto& input = quotes;
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, delim.at(0), '\n');
+	CSVParser parser(ss, delim.at(0));
 	checkParseResult(parser, input);
 }
 
@@ -111,7 +107,7 @@ TEST(CSVParser, lineBreaksLF) {
 	auto input = lineBreaksInQuotes(endl);
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, delim.at(0), '\n');
+	CSVParser parser(ss, delim.at(0));
 	checkParseResult(parser, input);
 
 }
@@ -122,7 +118,7 @@ TEST(CSVParser, lineBreaksCRLF) {
 	auto input = lineBreaksInQuotes(endl);
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, delim.at(0), '\n');
+	CSVParser parser(ss, delim.at(0));
 	checkParseResult(parser, input);
 }
 
@@ -132,7 +128,7 @@ TEST(CSVParser, delimInQuotesComma) {
 	auto input = delimInQuotes(delim);
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, delim.at(0), '\n');
+	CSVParser parser(ss, delim.at(0));
 	checkParseResult(parser, input);
 }
 
@@ -142,6 +138,6 @@ TEST(CSVParser, delimInQuotesSemicolon) {
 	auto input = delimInQuotes(delim);
 	std::stringstream ss;
 	toCSV(ss, input, delim, endl);
-	CSVParser parser(ss, delim.at(0), '\n');
+	CSVParser parser(ss, delim.at(0));
 	checkParseResult(parser, input);
 }

515b671830c028bbeac9a74106f2443047ee4899
mattig <mattig@mathematik.uni-marburg.de>
2016-02-16 17:49:20 +0100

    added a test for temporalreference intersect method

--- mapping/test/unittests/stref.cpp
+++ mapping/test/unittests/stref.cpp
@@ -1,4 +1,5 @@
-#include <datatypes/spatiotemporal.h>
+#include "datatypes/spatiotemporal.h"
+#include "util/timeparser.h"
 
 #include <string>
 #include <gtest/gtest.h>
@@ -11,4 +12,18 @@ TEST(STRef, toISOBeginEndOfTime) {
 
 	iso = tref.toIsoString(tref.end_of_time());
 	EXPECT_EQ("infinity", iso);
+}
+
+TEST(STRef, bla) {
+	auto parser = TimeParser::create(TimeParser::Format::ISO);
+
+	double time = parser->parse("2014-11-24T16:23:57");
+	double timeRef= parser->parse("2015-01-01T00:00:00");
+
+	TemporalReference tref(timetype_t::TIMETYPE_UNIX);
+
+	tref.t1 = timeRef;
+	tref.t2 = tref.end_of_time();
+
+	EXPECT_TRUE(tref.intersects(time, tref.end_of_time()));
 }

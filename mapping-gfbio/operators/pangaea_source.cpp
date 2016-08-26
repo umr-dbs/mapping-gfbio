54046bf1d3ad5304f41192387150577f544de67c
Michael Mattig <mattig@mathematik.uni-marburg.de>
2016-08-26 12:09:14 +0200

    PangaeaSource: use csv_soure_util instead of custom parsing

--- mapping-gfbio/operators/pangaea_source.cpp
+++ mapping-gfbio/operators/pangaea_source.cpp
@@ -1,13 +1,17 @@
 #include "operators/operator.h"
 #include "datatypes/pointcollection.h"
+
 #include "util/make_unique.h"
-#include <sstream>
-#include <json/json.h>
 #include "util/curl.h"
 #include "util/configuration.h"
-#include <boost/tokenizer.hpp>
-#include <limits>
+#include "util/csv_source_util.h"
+#include "util/timeparser.h"
 
+
+#include <vector>
+#include <limits>
+#include <sstream>
+#include <json/json.h>
 /**
  * Operator that gets points from pangaea
  *
@@ -18,20 +22,20 @@ class PangaeaSourceOperator : public GenericOperator {
 	public:
 		PangaeaSourceOperator(int sourcecounts[], GenericOperator *sources[], Json::Value &params) : GenericOperator(sourcecounts, sources) {
 			assumeSources(0);
-			doi = params.get("doi", "").asString();
+			dataLink = params.get("dataLink", "").asString();
 		}
 
 #ifndef MAPPING_OPERATOR_STUBS
 		virtual std::unique_ptr<PointCollection> getPointCollection(const QueryRectangle &rect, QueryProfiler &profiler);
 #endif
 		void writeSemanticParameters(std::ostringstream& stream) {
-			stream << "\"doi\":\"" << doi;
+			stream << "\"dataLink\":\"" << dataLink;
 		}
 
 		virtual ~PangaeaSourceOperator(){};
 
 	private:
-		std::string doi;
+		std::string dataLink;
 		cURL curl;
 
 #ifndef MAPPING_OPERATOR_STUBS
@@ -43,79 +47,35 @@ REGISTER_OPERATOR(PangaeaSourceOperator, "pangaea_source");
 
 #ifndef MAPPING_OPERATOR_STUBS
 std::unique_ptr<PointCollection> PangaeaSourceOperator::getPointCollection(const QueryRectangle &rect, QueryProfiler &profiler){
-	std::unique_ptr<PointCollection> points = make_unique<PointCollection>(rect);
-	fprintf(stderr, "x\n");
 	std::stringstream data;
 
 	getStringFromServer(data);
-	fprintf(stderr, "%s\n", data.str().c_str());
+	//fprintf(stderr, "%s\n", data.str().c_str());
 	//skip initial comment
+	// TODO: parse comment or retrieve metadata info in another way and extract parameters
+
 	size_t offset = data.str().find("*/\n") + 2;
 	std::string dataString = data.str().substr(offset);
-	fprintf(stderr, "y\n");
 
-	boost::char_delimiters_separator<char> lineSeparator(false, "", "\n"); //dont tokenize based on whitespace or .
-	boost::char_delimiters_separator<char> elementSeparator(false, "", "\t");
-	boost::tokenizer<boost::char_delimiters_separator<char>> lineTokenizer(dataString, lineSeparator);
+	std::istringstream iss(dataString);
 
-	bool atHeader = true;
-	size_t longitudeIndex = std::numeric_limits<size_t>::max();
-	size_t latitudeIndex = std::numeric_limits<size_t>::max();
+	char field_separator = '\t';
 
-	for(auto lineToken = lineTokenizer.begin(); lineToken != lineTokenizer.end(); ++lineToken){
-		const std::string& line = *lineToken;
-		//std::cout << line << std::endl;
-		boost::tokenizer<boost::char_delimiters_separator<char>> elementTokenizer(line, elementSeparator);
+	std::vector<std::string> columns_numeric;
+	std::vector<std::string> columns_string;
+	CSVSourceUtil csvUtil(GeometrySpecification::XY, TimeSpecification::NONE,
+			1.0, "Longitude", "Latitude", "", "", columns_numeric,
+			columns_string, field_separator, ErrorHandling::ABORT);
 
-		size_t columnIndex = 0;
-		Coordinate coordinate(0.0, 0.0);
-		//TODO: throw error if no lat/lon present
-		for(auto elementToken = elementTokenizer.begin(); elementToken != elementTokenizer.end(); ++elementToken){
-			const std::string& element = *elementToken;
-
-			if(atHeader){
-				std::string header = element;
-				//std::cout << header << std::endl;
-
-				std::transform(header.begin(), header.end(), header.begin(), ::tolower);
-				if(header.find("longitude") != std::string::npos){
-					longitudeIndex = columnIndex;
-				} else if(header.find("latitude") != std::string::npos){
-					latitudeIndex = columnIndex;
-				} else {
-					//TODO handle metadata
-				}
-			} else {
-				if(columnIndex == longitudeIndex){
-					coordinate.x = std::stod(element);
-				} else if (columnIndex == latitudeIndex){
-					coordinate.y = std::stod(element);
-				} else {
-					//TODO add metadata
-				}
-			}
-			columnIndex++;
-		}
-
-		if(atHeader ){
-			atHeader = false;
-		}
-		else {
-			points->addSinglePointFeature(coordinate);
-		}
-	}
+	auto points = csvUtil.getPointCollection(iss, rect);
 
 	return points;
 }
 
 void PangaeaSourceOperator::getStringFromServer(std::stringstream& data) {
-	std::ostringstream url;
-	url
-		<< "http://doi.pangaea.de/"
-		<< doi << "?format=textfile";
-	fprintf(stderr, "url: %s\n", url.str().c_str());
-	curl.setOpt(CURLOPT_PROXY, "http://www-proxy1.hrz.uni-marburg.de:3128");
-	curl.setOpt(CURLOPT_URL, url.str().c_str());
+	fprintf(stderr, "url: %s\n", dataLink.c_str());
+	curl.setOpt(CURLOPT_PROXY, Configuration::get("proxy", "").c_str());
+	curl.setOpt(CURLOPT_URL, dataLink.c_str());
 	curl.setOpt(CURLOPT_WRITEFUNCTION, cURL::defaultWriteFunction);
 	curl.setOpt(CURLOPT_WRITEDATA, &data);
 

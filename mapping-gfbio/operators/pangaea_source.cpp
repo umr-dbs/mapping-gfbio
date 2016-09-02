1254b9a953c5c6be0ac93a7bf06a86c331509407
Michael Mattig <mattig@mathematik.uni-marburg.de>
2016-09-02 15:12:42 +0200

    PangaeaSource: parse ProvenanceInfo from DataDescription
    Makefile: link to boost_regex, because std::regex can not handle the regex

--- mapping-gfbio/operators/pangaea_source.cpp
+++ mapping-gfbio/operators/pangaea_source.cpp
@@ -6,12 +6,16 @@
 #include "util/configuration.h"
 #include "util/csv_source_util.h"
 #include "util/timeparser.h"
+#include "util/csvparser.h"
 
 
 #include <vector>
 #include <limits>
 #include <sstream>
+#include <iostream>
 #include <json/json.h>
+#include <boost/regex.hpp>
+
 /**
  * Operator that gets points from pangaea
  *
@@ -20,13 +24,13 @@
  */
 class PangaeaSourceOperator : public GenericOperator {
 	public:
-		PangaeaSourceOperator(int sourcecounts[], GenericOperator *sources[], Json::Value &params) : GenericOperator(sourcecounts, sources) {
-			assumeSources(0);
-			dataLink = params.get("dataLink", "").asString();
-		}
+		PangaeaSourceOperator(int sourcecounts[], GenericOperator *sources[], Json::Value &params);
 
 #ifndef MAPPING_OPERATOR_STUBS
 		virtual std::unique_ptr<PointCollection> getPointCollection(const QueryRectangle &rect, QueryProfiler &profiler);
+		virtual void getProvenance(ProvenanceCollection &pc);
+
+		void parseDataDescription(std::string& dataDescription);
 #endif
 		void writeSemanticParameters(std::ostringstream& stream) {
 			stream << "\"dataLink\":\"" << dataLink;
@@ -38,6 +42,10 @@ class PangaeaSourceOperator : public GenericOperator {
 		std::string dataLink;
 		cURL curl;
 
+		std::string citation;
+		std::string license;
+		std::string uri;
+
 #ifndef MAPPING_OPERATOR_STUBS
 		void getStringFromServer(std::stringstream& data);
 #endif
@@ -45,17 +53,58 @@ class PangaeaSourceOperator : public GenericOperator {
 REGISTER_OPERATOR(PangaeaSourceOperator, "pangaea_source");
 
 
+PangaeaSourceOperator::PangaeaSourceOperator(int sourcecounts[], GenericOperator *sources[], Json::Value &params) : GenericOperator(sourcecounts, sources) {
+	assumeSources(0);
+	dataLink = params.get("dataLink", "").asString();
+
+
+}
+
+
+
+
 #ifndef MAPPING_OPERATOR_STUBS
+
+void PangaeaSourceOperator::parseDataDescription(std::string& dataDescription) {
+
+	boost::regex regex(R"(Citation:\t([^:]+\(\d+\)): ([^\n]+)[.,;] doi:([PANGE0-9.\/]+).*\n.*License:\t([^\n]+)\n)");
+
+	boost::smatch sm;
+
+	if(boost::regex_search (dataDescription, sm, regex)) {
+		for (size_t i = 0; i < sm.size(); ++i) {
+			fprintf(stderr, ">%s<", sm[i].str().c_str());
+		}
+
+		citation = sm[1].str() + ": " + sm[2].str();
+		uri = "https://doi.pangaea.de/" + sm[3].str();
+		license = sm[4].str();
+	}
+}
+
+
 std::unique_ptr<PointCollection> PangaeaSourceOperator::getPointCollection(const QueryRectangle &rect, QueryProfiler &profiler){
 	std::stringstream data;
 
 	getStringFromServer(data);
-	//fprintf(stderr, "%s\n", data.str().c_str());
-	//skip initial comment
-	// TODO: parse comment or retrieve metadata info in another way and extract parameters
 
+
+	//skip initial comment
 	size_t offset = data.str().find("*/\n") + 2;
 	std::string dataString = data.str().substr(offset);
+
+	// extract headers
+	// TODO: the mapping of the columns should be done on client side
+	{
+		std::istringstream headerStream(dataString);
+
+		CSVParser csvParser(headerStream, '\t');
+		std::vector<std::string> headers = csvParser.readHeaders();
+
+		for(auto header : headers) {
+//			fprintf(stderr, header.c_str());
+		}
+	}
 
 	std::istringstream iss(dataString);
 
@@ -73,12 +122,28 @@ std::unique_ptr<PointCollection> PangaeaSourceOperator::getPointCollection(const
 }
 
 void PangaeaSourceOperator::getStringFromServer(std::stringstream& data) {
-	fprintf(stderr, "url: %s\n", dataLink.c_str());
+//	fprintf(stderr, "url: %s\n", dataLink.c_str());
 	curl.setOpt(CURLOPT_PROXY, Configuration::get("proxy", "").c_str());
 	curl.setOpt(CURLOPT_URL, dataLink.c_str());
 	curl.setOpt(CURLOPT_WRITEFUNCTION, cURL::defaultWriteFunction);
 	curl.setOpt(CURLOPT_WRITEDATA, &data);
 
 	curl.perform();
+}
+
+void PangaeaSourceOperator::getProvenance(ProvenanceCollection &pc) {
+	// TODO avoid loading ALL data again
+	std::stringstream data;
+
+	getStringFromServer(data);
+
+	// skip initial comment
+	size_t offset = data.str().find("*/\n") + 2;
+	std::string dataDescription = data.str().substr(0, offset);
+	fprintf(stderr, dataDescription.c_str());
+
+	parseDataDescription(dataDescription);
+
+	pc.add(Provenance(citation, license, uri, "data.pangaea." + dataLink));
 }
 #endif

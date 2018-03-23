6b6cb86925df4c573e50c6735debfb6f9c5f7173
Sören Hoffstedt <hoffsted@students.uni-marburg.de>
2018-03-23 10:32:48 +0100

    Clean up

--- src/operators/terminology_resolver.cpp
+++ src/operators/terminology_resolver.cpp
@@ -13,9 +13,9 @@
  * parameters:
  *  - column: name of the textual column to resolve
  *  - terminology: the terminology used to resolve
- *  - name_appendix:
+ *  - name_appendix: name of the next column for the resolved text values
  *      - "terminology": name will be "column terminology" with column and terminology being the parameters above
- *      - a custom_string: name will be "column custom_string" with column being the parameter above.
+ *      - a custom string: name will be the custom string.
  *      - if not provided the name will be "column resolved" with column being the parameter above.
  *  - on_not_resolvable: if no label for the string was found, what to insert into new column
  *      - "EMPTY"
@@ -25,12 +25,14 @@
 class TerminologyResolver : public GenericOperator {
 public:
     TerminologyResolver(int *sourcecounts, GenericOperator **sources, Json::Value &params);
+    ~TerminologyResolver() override;
 
-    std::unique_ptr<PointCollection> getPointCollection(const QueryRectangle &rect, const QueryTools &tools) override;
+    std::unique_ptr<PointCollection>    getPointCollection(const QueryRectangle &rect, const QueryTools &tools) override;
+    std::unique_ptr<LineCollection>     getLineCollection(const QueryRectangle &rect, const QueryTools &tools) override;
+    std::unique_ptr<PolygonCollection>  getPolygonCollection(const QueryRectangle &rect, const QueryTools &tools) override;
 
-    std::unique_ptr<LineCollection> getLineCollection(const QueryRectangle &rect, const QueryTools &tools) override;
-
-    std::unique_ptr<PolygonCollection> getPolygonCollection(const QueryRectangle &rect, const QueryTools &tools) override;
+protected:
+    void writeSemanticParameters(std::ostringstream& stream) override;
 
 private:
     std::string column;
@@ -40,22 +42,19 @@ private:
 
 };
 
-REGISTER_OPERATOR(TerminologyResolver, "terminology_resolver");
-
-
 TerminologyResolver::TerminologyResolver(int *sourcecounts, GenericOperator **sources, Json::Value &params)
-        : GenericOperator(sourcecounts, sources) {
+        : GenericOperator(sourcecounts, sources)
+{
     column          = params.get("column", "").asString();
     terminology     = params.get("terminology", "").asString();
 
     std::string name_appendix = params.get("name_appendix", "").asString();
-
     if(name_appendix.empty())
         new_column = column + " resolved";
     else if(name_appendix == "terminology")
         new_column = column + " " + terminology;
     else
-        new_column = column + " " + name_appendix;
+        new_column = name_appendix;
 
     std::string not_resolvable = params.get("on_not_resolvable", "").asString();
     if(not_resolvable == "EMPTY")
@@ -63,31 +62,31 @@ TerminologyResolver::TerminologyResolver(int *sourcecounts, GenericOperator **so
     else if(not_resolvable == "OLD_NAME")
         on_not_resolvable = OLD_NAME;
     else
-        throw ArgumentException("Terminology Resolver: on_not_resolvable has not valid value: " + not_resolvable);
+        throw ArgumentException("Terminology Resolver: on_not_resolvable was not a valid value: " + not_resolvable + ". Must be EMPTY or OLD_NAME.");
 }
+
+TerminologyResolver::~TerminologyResolver() { }
+
+REGISTER_OPERATOR(TerminologyResolver, "terminology_resolver");
 
 std::unique_ptr<PointCollection>
 TerminologyResolver::getPointCollection(const QueryRectangle &rect, const QueryTools &tools) {
-
     auto points = getPointCollectionFromSource(0, rect, tools);
 
     auto &old_attribute_array = points->feature_attributes.textual(column);
-    //create new textual column and fill it with resolved labels from the Terminology class
-    auto &new_vector = points->feature_attributes.addTextualAttribute(new_column, old_attribute_array.unit);
-    Terminology::request_labels(old_attribute_array.array, new_vector.array, terminology, on_not_resolvable);
+    auto &new_attribute_array = points->feature_attributes.addTextualAttribute(new_column, old_attribute_array.unit);
+    Terminology::requestLabels(old_attribute_array.array, new_attribute_array.array, terminology, on_not_resolvable);
 
     return points;
 }
 
 std::unique_ptr<LineCollection>
 TerminologyResolver::getLineCollection(const QueryRectangle &rect, const QueryTools &tools) {
-
     auto lines = getLineCollectionFromSource(0, rect, tools);
 
     auto &old_attribute_array = lines->feature_attributes.textual(column);
-    //create new textual column and fill it with resolved labels from the Terminology class
-    auto &new_vector = lines->feature_attributes.addTextualAttribute(new_column, old_attribute_array.unit);
-    Terminology::request_labels(old_attribute_array.array, new_vector.array, terminology, on_not_resolvable);
+    auto &new_attribute_array = lines->feature_attributes.addTextualAttribute(new_column, old_attribute_array.unit);
+    Terminology::requestLabels(old_attribute_array.array, new_attribute_array.array, terminology, on_not_resolvable);
 
     return lines;
 }
@@ -97,9 +96,20 @@ TerminologyResolver::getPolygonCollection(const QueryRectangle &rect, const Quer
     auto polygons = getPolygonCollectionFromSource(0, rect, tools);
 
     auto &old_attribute_array = polygons->feature_attributes.textual(column);
-    //create new textual column and fill it with resolved labels from the Terminology class
-    auto &new_vector = polygons->feature_attributes.addTextualAttribute(new_column, old_attribute_array.unit);
-    Terminology::request_labels(old_attribute_array.array, new_vector.array, terminology, on_not_resolvable);
+    auto &new_attribute_array = polygons->feature_attributes.addTextualAttribute(new_column, old_attribute_array.unit);
+    Terminology::requestLabels(old_attribute_array.array, new_attribute_array.array, terminology, on_not_resolvable);
 
     return polygons;
 }
+
+void TerminologyResolver::writeSemanticParameters(std::ostringstream &stream) {
+    Json::Value json(Json::objectValue);
+
+    json["column"]              = column;
+    json["terminology"]         = terminology;
+    json["new_column"]          = new_column;
+    json["on_not_resolvable"]   = (on_not_resolvable == EMPTY) ? "EMPTY" : "NOT_EMPTY";
+
+    stream << json;
+}
+
--- src/util/terminology.cpp
+++ src/util/terminology.cpp
@@ -12,24 +12,66 @@
 #include <boost/asio.hpp>
 #include <boost/move/move.hpp>
 
-void Terminology::request_labels(const std::vector<std::string> &names_in,
-                                 std::vector<std::string> &names_out,
-                                 const std::string &terminology,
-                                 const HandleNotResolvable on_not_resolvable){
-
-    request_labels_thread_pool(names_in, names_out, terminology, on_not_resolvable);
-}
-
-
 typedef boost::packaged_task<std::string> task_t;
 typedef boost::shared_ptr<task_t> ptask_t;
 
-const std::string TERMINOLOGIES_SEARCH_BASE_URI = "https://terminologies.gfbio.org/api/terminologies/search";
+void Terminology::requestLabels(const std::vector<std::string> &names_in,
+                                std::vector<std::string> &names_out,
+                                const std::string &terminology,
+                                const HandleNotResolvable on_not_resolvable){
 
-std::string Terminology::resolve_single_name(Poco::Net::Context::Ptr &context, Poco::Net::Session::Ptr &session_ptr,
-                                             std::string &name, std::string &terminology,
-                                             HandleNotResolvable &on_not_resolvable){
-    Poco::URI uri(TERMINOLOGIES_SEARCH_BASE_URI);
+    if(names_in.empty())
+        return;
+
+    //init thread pool
+    const int threads_num = Configuration::get<int>("terminology.threads",16);
+
+    boost::asio::io_service io_service;
+    boost::thread_group threads;
+    boost::asio::io_service::work work(io_service);
+
+    for(int i = 0; i < threads_num; i++){
+        threads.create_thread(boost::bind(&boost::asio::io_service::run, &io_service));
+    }
+
+    std::vector<boost::shared_future<std::string>> pending_results;
+
+    // create context, enable session cache. First name has to be resolved seperately and not in thread to
+    // set the session ptr. Because the Session::Ptr can not be assigned a new value the local session_ptr
+    // variable is passed as a raw pointer to the method.
+    Poco::Net::Context::Ptr context = new Poco::Net::Context(
+            Poco::Net::Context::CLIENT_USE,
+            "",
+            Poco::Net::Context::VERIFY_RELAXED,
+            9,
+            true
+    );
+    context->enableSessionCache(true);
+    Poco::Net::Session::Ptr session_ptr;
+    std::string first_resolved = resolveSingleNameSetSessionPtr(context, &session_ptr, names_in[0], terminology,
+                                                                on_not_resolvable);
+    names_out.push_back(first_resolved);
+
+    //push tasks to resolve all names into the thread pool
+    for(int i = 1; i < names_in.size(); i++) {
+        const std::string &name = names_in[i];
+        ptask_t task = boost::make_shared<task_t>(
+                boost::bind(resolveSingleName, context, session_ptr, name, terminology, on_not_resolvable));
+        boost::shared_future<std::string> future(task->get_future());
+        pending_results.push_back(future);
+        io_service.post(boost::bind(&task_t::operator(), task));
+    }
+
+    //get the results from the futures
+    for(auto &future : pending_results){
+        names_out.push_back(future.get());
+    }}
+
+
+std::string Terminology::resolveSingleName(Poco::Net::Context::Ptr &context, Poco::Net::Session::Ptr &session_ptr,
+                                           std::string &name, std::string &terminology,
+                                           HandleNotResolvable &on_not_resolvable){
+    Poco::URI uri("https://terminologies.gfbio.org/api/terminologies/search");
     uri.addQueryParameter("query", name);
     uri.addQueryParameter("terminologies", terminology);
 
@@ -61,14 +103,13 @@ std::string Terminology::resolve_single_name(Poco::Net::Context::Ptr &context, P
     }
 }
 
-std::string
-Terminology::resolve_single_name_set_session_ptr(Poco::Net::Context::Ptr &context,
-                                                 Poco::Net::Session::Ptr *session_ptr,
-                                                 const std::string &name,
-                                                 const std::string &terminology,
-                                                 const HandleNotResolvable on_not_resolvable) {
+std::string Terminology::resolveSingleNameSetSessionPtr(Poco::Net::Context::Ptr &context,
+                                                        Poco::Net::Session::Ptr *session_ptr,
+                                                        const std::string &name,
+                                                        const std::string &terminology,
+                                                        HandleNotResolvable on_not_resolvable) {
 
-    Poco::URI uri(TERMINOLOGIES_SEARCH_BASE_URI);
+    Poco::URI uri("https://terminologies.gfbio.org/api/terminologies/search");
     uri.addQueryParameter("query", name);
     uri.addQueryParameter("terminologies", terminology);
 
@@ -103,59 +144,7 @@ Terminology::resolve_single_name_set_session_ptr(Poco::Net::Context::Ptr &contex
     }
 }
 
-
-
-void Terminology::request_labels_thread_pool(const std::vector<std::string> &names_in,
-                                             std::vector<std::string> &names_out,
-                                             const std::string &terminology,
-                                             const HandleNotResolvable on_not_resolvable){
-
-    if(names_in.size() == 0)
-        return;
-
-    //init thread pool
-    const int threads_num = Configuration::get<int>("terminology.threads",16);
-
-    boost::asio::io_service io_service;
-    boost::thread_group threads;
-    boost::asio::io_service::work work(io_service);
-
-    for(int i = 0; i < threads_num; i++){
-        threads.create_thread(boost::bind(&boost::asio::io_service::run, &io_service));
-    }
-
-    std::vector<boost::shared_future<std::string>> pending_results;
-
-
-    Poco::Net::Context::Ptr context = new Poco::Net::Context(
-            Poco::Net::Context::CLIENT_USE,
-            "",
-            Poco::Net::Context::VERIFY_RELAXED,
-            9,
-            true
-    );
-    context->enableSessionCache(true);
-    Poco::Net::Session::Ptr session_ptr;
-    names_out.push_back(
-            resolve_single_name_set_session_ptr(context, &session_ptr, names_in[0], terminology, on_not_resolvable));
-
-    std::cout << "is session ptr null: " << (session_ptr.isNull() ? "true" : "false") << std::endl;
-
-    for(int i = 1; i < names_in.size(); i++) {
-        const std::string &name = names_in[i];
-        ptask_t task = boost::make_shared<task_t>(
-                boost::bind(resolve_single_name, context, session_ptr, name, terminology, on_not_resolvable));
-        boost::shared_future<std::string> future(task->get_future());
-        pending_results.push_back(future);
-        io_service.post(boost::bind(&task_t::operator(), task));
-    }
-
-    for(auto &future : pending_results){
-        names_out.push_back(future.get());
-    }
-
-}
-
+/*
 void Terminology::requestLabelsBatch(const std::vector<std::string> &names_in,
                                std::vector<std::string> &names_out, const std::string &terminology,
                                const HandleNotResolvable onNotResolvable){
@@ -230,7 +219,8 @@ void Terminology::requestLabelsBatch(const std::vector<std::string> &names_in,
 
     }
 }
-
+ */
+/*
 void Terminology::requestLabelsAllConcurrent(const std::vector<std::string> &names_in,
                                              std::vector<std::string> &names_out,
                                              const std::string &terminology,
@@ -283,31 +273,16 @@ void Terminology::requestLabelsAllConcurrent(const std::vector<std::string> &nam
     }
 
 }
+*/
 
-std::string Terminology::request_label(const std::string &name,
-                                       const std::string &terminology,
-                                       const HandleNotResolvable onNotResolvable){
-
+std::string Terminology::requestSingleLabel(const std::string &name,
+                                            const std::string &terminology,
+                                            HandleNotResolvable onNotResolvable)
+{
     Poco::URI uri("https://terminologies.gfbio.org/api/terminologies/search");
     uri.addQueryParameter("query", name);
     uri.addQueryParameter("terminologies", terminology);
-    Json::Value response = get_https_response(uri);
 
-    std::string not_resolved = (onNotResolvable == EMPTY) ? "" : name;
-
-    if (response.isNull())
-    {
-        return not_resolved;
-    } else
-    {
-        Json::Value results = response["results"];
-        Json::Value first = results[0];
-        return first.get("label", not_resolved).asString();
-    }
-
-}
-
-Json::Value Terminology::get_https_response(Poco::URI uri) {
     Poco::Net::HTTPSClientSession session(uri.getHost(), uri.getPort());
     Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET, uri.getPathAndQuery(), Poco::Net::HTTPRequest::HTTP_1_1);
     Poco::Net::HTTPResponse response;
@@ -315,13 +290,23 @@ Json::Value Terminology::get_https_response(Poco::URI uri) {
     session.sendRequest(request);
     std::istream& respStream = session.receiveResponse(response);
 
+    Json::Value response_json;
     if (response.getStatus() == Poco::Net::HTTPResponse::HTTP_OK)
     {
-        Json::Value root;
-        respStream >> root;
-        return root;
+        respStream >> response_json;
     } else {
-        return Json::Value::null;
+        response_json = Json::Value::null;
+    }
+
+    std::string not_resolved = (onNotResolvable == EMPTY) ? "" : name;
+
+    if (response_json.isNull())
+    {
+        return not_resolved;
+    } else
+    {
+        Json::Value results = response_json["results"];
+        Json::Value first = results[0];
+        return first.get("label", not_resolved).asString();
     }
 }
-

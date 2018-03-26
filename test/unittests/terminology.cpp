3b1395c8fe851749581c9325bd146ff5b5c21bae
Sören Hoffstedt <hoffsted@students.uni-marburg.de>
2018-03-26 13:13:16 +0200

    Test Data for Terminology Resolver.

---
+++ test/unittests/terminology.cpp
@@ -0,0 +1,39 @@
+
+#include "../../../../mapping-gfbio/src/util/terminology.h"
+#include <gtest/gtest.h>
+
+
+TEST(Terminology, requestLabel){
+  std::string first = "plum";
+    std::string second = "honey bee";
+    std::string term1 = "NCBITAXON";
+    std::string term2 = "PESI";
+    EXPECT_EQ(Terminology::resolveSingle(first, term1, "label", EMPTY), "Prunus domestica");
+    EXPECT_EQ(Terminology::resolveSingle(second, term2, "label", EMPTY), "Apis mellifera Linnaeus, 1758");
+
+}
+
+
+TEST(Terminology, multiplerequests){
+    std::string terminology("NCBITAXON");
+    std::vector<std::string> names_in;
+    const int num = 50;
+    for(int i = 0; i < num; i++){
+        names_in.emplace_back("plum");
+        names_in.emplace_back("honey bee");
+    }
+    names_in.emplace_back("dose");
+
+    std::vector<std::string> names_out;
+
+    Terminology::resolveMultiple(names_in, names_out, terminology, "label", EMPTY);
+
+    EXPECT_EQ(names_out.size(), num*2+1);
+    for(int i = 0; i < num; i++) {
+        EXPECT_EQ(names_out[i * 2], "Prunus domestica");
+        EXPECT_EQ(names_out[i * 2 + 1], "Apis mellifera");
+    }
+    EXPECT_EQ(names_out[num*2], "");
+
+}
+
